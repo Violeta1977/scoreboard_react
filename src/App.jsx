7c6bf15c6142888a72720fa485d038acda7a1cd4
VioletaS <violistancikiene@gmail.com>
2024-08-07 19:08:44 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// import { ButtonsList } from "./components/buttons/ButtonsList"
+import { Score } from "./components/score/Score"
+
+
+function App() {
+  
+  return (
+    <>
+      <div className="scoreboard">
+        <h1>Bascetball scoreboard</h1>
+        < Score />
+         {/* <ButtonsList/> */}
+      </div>
+    </>
+  )
+}
+
+export default App
